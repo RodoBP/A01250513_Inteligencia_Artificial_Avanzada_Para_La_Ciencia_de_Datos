2ceb171eda742012610521c9377395c6d8f56b5b
Rodolfo Bojorquez <rodobp97@gmail.com>
2023-09-19 14:32:58 -0600

    Adding folders

---
+++ Software/inicio.py
@@ -0,0 +1,17 @@
+string1 = "Rodo"
+string2 = "Shemet"
+string3 = "Karen"
+string4 = "Clase"
+bool1 = True
+bool2 = False
+bool3 = None
+int1 = 1
+int2 = 500
+int3 = 21354
+int4 = -1
+float1 = 1.0
+float2 = 0.9
+float3 = 2.5
+float4 = 3.3
+
+array1 = [1,2,3,4]
---
+++ Software/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.array([[2,3,5],[2, 4, 2]])
+
+print(a)
